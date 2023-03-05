3f048b398045b103a1b4610d59c3a3123c837abd
sh1nn0on3 <sanogaku31@gmail.com>
2023-03-06 05:22:03 +0700

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="pokemon-header">Pokemon</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
