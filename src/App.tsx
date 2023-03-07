3a48b6bf01686aed34491d1516bb923e0de744a1
sh1nn0on3 <sanogaku31@gmail.com>
2023-03-07 16:28:51 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [nextUrl, setNextUrl] = useState<string>("1");
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const poke = await axios
+        .get(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${nextUrl}`)
+        .then((res) => {
+          console.log("sucessful !!!");
+          // console.log("🚀 ~ file: App.tsx:14 ~ .then ~ res:", res.data.results);
+
+          res.data.results.forEach(async (pokemon: any) => {
+            const poke = await axios.get(``).then().catch();
+          });
+        })
+        .catch();
+    };
+
+    return () => {
+      getPokemon();
+    };
+  }, [nextUrl]);
+
   return (
     <div className="App">
       <div className="container">
